6e19fb6fabb61167a0fa00acbdf110295a811260
RCJamen <rcjamen10@gmail.com>
2023-04-12 00:25:24 +0800

    Edit Gig

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,11 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 // Store Listing Data
 Route::post('/listings', [ListingController::class, 'store']);
 
+// Show edit form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
+// Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
-//Single Listing
+// Single Listing
 Route::get('listings/{listing}', [ListingController::class, 'show']);
